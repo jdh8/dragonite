870d4feef5d03bfbb0ee8b67144121e7a8c68e84
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-28 00:10:00 +0800

    Implement softmax filter

--- dragonite.py
+++ dragonite.py
@@ -3,11 +3,18 @@ import numpy
 import sys
 
 with open(sys.argv[2], "w") as stream:
+    def softmax(x, *, axis=1):
+        axes = *range(axis, x.ndim),
+        fmax = numpy.fmax.reduce(x, axis=axes, keepdims=True)
+        exp = numpy.exp(x - fmax)
+        return exp / numpy.sum(exp, axis=axes, keepdims=True)
+
     environment = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
 
     environment.globals["gemm"] = lambda x, y: x @ y
     environment.globals["numpy"] = numpy
 
     environment.filters["flatten"] = lambda x: "{" + ",".join(map(str, x.flatten())) + "}"
+    environment.filters["softmax"] = softmax
 
     stream.write(environment.get_template(sys.argv[1]).render())
