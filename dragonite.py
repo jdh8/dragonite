c970ac29461aa1a799b894fe7c2b56b8d148d315
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-27 23:39:57 +0800

    Move global variables into a block to avoid accidental export

--- dragonite.py
+++ dragonite.py
@@ -2,12 +2,12 @@ import jinja2
 import numpy
 import sys
 
-environment = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
+with open(sys.argv[2], "w") as stream:
+    environment = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
 
-environment.globals["gemm"] = lambda x, y: x @ y
-environment.globals["numpy"] = numpy
+    environment.globals["gemm"] = lambda x, y: x @ y
+    environment.globals["numpy"] = numpy
 
-environment.filters["flatten"] = lambda x: "{" + ",".join(map(str, x.flatten())) + "}"
+    environment.filters["flatten"] = lambda x: "{" + ",".join(map(str, x.flatten())) + "}"
 
-with open(sys.argv[2], "w") as f:
-    f.write(environment.get_template(sys.argv[1]).render())
+    stream.write(environment.get_template(sys.argv[1]).render())
