022afa4be073ce1974f53ad1a8c1485dfed307a8
Chen-Pang He <chen.pang.he@jdh8.org>
2019-06-29 17:56:45 +0800

    Iterate over subspaces in PRelu test to make it robust and concise

--- dragonite.py
+++ dragonite.py
@@ -1,13 +1,17 @@
 import jinja2
 import numpy
+
+import itertools
 import sys
 
 with open(sys.argv[2], "w") as stream:
     environment = jinja2.Environment(loader = jinja2.FileSystemLoader([".", "template"]))
+    environment.globals["itertools"] = itertools
     environment.globals["numpy"] = numpy
 
     environment.filters["array"] = lambda x: "{ " + (", ".join(map(str, x)) or "dragonite::canary") + " }"
     environment.filters["gemm"] = lambda x, y: x @ y
+    environment.filters["star"] = lambda x, f, *args, **keys: f(*x, *args, **keys)
     environment.filters["tuple"] = tuple
 
     stream.write(environment.get_template(sys.argv[1]).render())
--- template/PRelu.cpp
+++ template/PRelu.cpp
@@ -1,59 +1,46 @@
 #include "dragonite.hpp"
 
-{% macro testcase(name, x, slope) -%}
+{% macro subtest(x, subshape) -%}
+{
+    {% set slope = numpy.random.sample(size = subshape).astype(numpy.float32) -%}
+
+    const float slope[] = {{ slope.flatten() | array }};
+    const float y[] = {{ numpy.where(numpy.signbit(x), slope * x, x).flatten() | array }};
+
+    const std::int32_t subshape[] = {{ subshape | array }};
+
+    ONNC_RUNTIME_prelu_float(nullptr, x, ndim, shape, slope, ndim, subshape, buffer, ndim, shape);
+    ASSERT_TRUE(dragonite::approx(buffer, y, size));
+
+    for (std::int32_t squeezed = 1; squeezed <= ndim && subshape[squeezed - 1] == 1; ++squeezed) {
+        ONNC_RUNTIME_prelu_float(nullptr, x, ndim, shape, slope, ndim - squeezed, subshape + squeezed, buffer, ndim, shape);
+        ASSERT_TRUE_MSG(dragonite::approx(buffer, y, size), squeezed);
+    }
+}
+{% endmacro -%}
+
+{% macro testcase(name, shape) -%}
 SKYPAT_F(PRelu, {{ name }})
 {
+    {% set x = numpy.random.normal(size = shape).astype(numpy.float32) -%}
+    {% set ndim = shape | length %}
+
     const float x[] = {{ x.flatten() | array }};
-    const float slope[] = {{ slope.flatten() | array }};
-    const float y[] = {{ numpy.where(numpy.signbit(x), numpy.broadcast_to(slope, x.shape) * x, x).flatten() | array }};
 
-    const std::int32_t shape[] = {{ x.shape | array }};
-    const std::int32_t slope_shape[] = {{ slope.shape | array }};
-
-    const std::int32_t ndim = {{ x.ndim }};
+    const std::int32_t shape[] = {{ shape | array }};
+    const std::int32_t ndim = {{ ndim }};
     const std::size_t size = {{ x.size }};
 
     float buffer[size];
 
-    ONNC_RUNTIME_prelu_float(nullptr, x, ndim, shape, slope, {{ slope.ndim }}, slope_shape, buffer, ndim, shape);
-
-    ASSERT_TRUE(dragonite::approx(buffer, y, size));
+    {% for bits in (ndim * ((1, 0),)) | star(itertools.product) -%}
+        {{ subtest(x, numpy.power(shape, bits)) | indent }}
+    {% endfor %}
 }
 {% endmacro -%}
 
-{% set shape = numpy.random.randint(2, 6, numpy.random.randint(3, 6)) %}
-
-{{ testcase("ScalarScalar",
-    numpy.array(numpy.float32(numpy.random.randn())),
-    numpy.float32(numpy.random.rand())) }}
-{{ testcase("VectorScalar",
-    numpy.random.normal(size = shape[:1]).astype(numpy.float32),
-    numpy.float32(numpy.random.rand())) }}
-{{ testcase("MatrixScalar",
-    numpy.random.normal(size = shape[:2]).astype(numpy.float32),
-    numpy.float32(numpy.random.rand())) }}
-{{ testcase("TensorScalar",
-    numpy.random.normal(size = shape).astype(numpy.float32),
-    numpy.float32(numpy.random.rand())) }}
-
-{{ testcase("VectorVector",
-    numpy.random.normal(size = shape[3]).astype(numpy.float32),
-    numpy.random.sample(size = shape[3]).astype(numpy.float32)) }}
-{{ testcase("MatrixVector",
-    numpy.random.normal(size = shape[:2]).astype(numpy.float32),
-    numpy.random.sample(size = shape[1]).astype(numpy.float32)) }}
-{{ testcase("TensorVector",
-    numpy.random.normal(size = shape).astype(numpy.float32),
-    numpy.random.sample(size = shape[-1:]).astype(numpy.float32)) }}
-
-{{ testcase("MatrixMatrix",
-    numpy.random.normal(size = shape[:2]).astype(numpy.float32),
-    numpy.random.sample(size = shape[:2]).astype(numpy.float32)) }}
-{{ testcase("TensorMatrix",
-    numpy.random.normal(size = shape).astype(numpy.float32),
-    numpy.random.sample(size = shape[-2:]).astype(numpy.float32)) }}
-
-{{ testcase("TensorTensor",
-    numpy.random.normal(size = shape).astype(numpy.float32),
-    numpy.random.sample(size = shape).astype(numpy.float32)) -}}
+{{ testcase("scalar", ()) }}
+{{ testcase("vector", numpy.random.randint(2, 6, 1)) }}
+{{ testcase("matrix", numpy.random.randint(2, 6, 2)) }}
+{{ testcase("tensor", numpy.random.randint(2, 6, numpy.random.randint(3, 6))) -}}
 {# vim: set ft=liquid: #}
