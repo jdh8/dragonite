cead64425013eff23e88757592e6d91a163a34a7
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-03 02:08:27 +0800

    Figure out testing distribution for Pow

---
+++ template/Pow.cpp
@@ -0,0 +1,16 @@
+#include "dragonite/common.hpp"
+
+{% from "mod/batch.cpp" import batch -%}
+{% from "mod/binary.cpp" import inner, outer -%}
+
+{% call(name, shape) batch() -%}
+    {% call(x, y) outer("Pow", name, shape) -%}
+        {% set r = numpy.random.rayleigh(size=x).astype(numpy.float32) -%}
+        {% set a = numpy.random.randn(*y).astype(numpy.float32) -%}
+        {{ inner(r**a, r, a) }}
+
+        {% set b = numpy.random.randint(0, 16, y, numpy.int8) -%}
+        {{ inner((-r)**b, -r, b) }}
+    {% endcall -%}
+{% endcall -%}
+{# vim: set ft=liquid: #}
