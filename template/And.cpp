649b677b5331c1899c6a21fb76edac7e9139c2b1
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-03 10:53:26 +0800

    Test bitwise/logical operators

---
+++ template/And.cpp
@@ -0,0 +1,13 @@
+#include "dragonite/common.hpp"
+
+{% from "mod/batch.cpp" import batch -%}
+{% from "mod/binary.cpp" import inner, outer -%}
+
+{% call(name, shape) batch() -%}
+    {% call(x, y) outer("And", name, shape) -%}
+        {% set a = numpy.random.randint(0, 2, x, numpy.int8) -%}
+        {% set b = numpy.random.randint(0, 2, y, numpy.int8) -%}
+        {{ inner(numpy.bitwise_and(a, b), a, b) }}
+    {% endcall -%}
+{% endcall -%}
+{# vim: set ft=liquid: #}
