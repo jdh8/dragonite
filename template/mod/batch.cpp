28452712effbce44d4ae84a225b5dffe2c681b7e
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-05 22:28:55 +0800

    Prepend optional batch and channel dimensions

--- template/mod/batch.cpp
+++ template/mod/batch.cpp
@@ -1,7 +1,7 @@
-{% macro batch() -%}
-{{ caller("vector", numpy.random.randint(2, 6, 1)) }}
-{{ caller("matrix", numpy.random.randint(2, 6, 2)) }}
-{{ caller("tensor", numpy.random.randint(2, 6, numpy.random.randint(3, 6))) }}
-{{ caller("scalar", ()) -}}
+{% macro batch(ndim=0) -%}
+{{ caller("vector", numpy.random.randint(2, 6, ndim + 1)) }}
+{{ caller("matrix", numpy.random.randint(2, 6, ndim + 2)) }}
+{{ caller("tensor", numpy.random.randint(2, 6, ndim + numpy.random.randint(3, 6))) }}
+{{ caller("scalar", numpy.random.randint(2, 6, ndim)) -}}
 {% endmacro -%}
 {# vim: set ft=liquid: #}
