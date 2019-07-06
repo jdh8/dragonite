132a59dea6532ef266558f52af591bc25678c93c
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-07 03:15:32 +0800

    Stop testing tensors with order > 6 to avoid unnecessary curse of dimensionality

--- template/mod/batch.cpp
+++ template/mod/batch.cpp
@@ -1,7 +1,7 @@
 {% macro batch(ndim=0) -%}
 {{ caller("vector", numpy.random.randint(2, 6, ndim + 1)) }}
 {{ caller("matrix", numpy.random.randint(2, 6, ndim + 2)) }}
-{{ caller("tensor", numpy.random.randint(2, 6, ndim + numpy.random.randint(3, 6))) }}
+{{ caller("tensor", numpy.random.randint(2, 6, numpy.random.randint(ndim + 3, 6))) }}
 {{ caller("scalar", numpy.random.randint(2, 6, ndim)) -}}
 {% endmacro -%}
 {# vim: set ft=liquid: #}
