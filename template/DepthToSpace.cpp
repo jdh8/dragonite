efddce0d5b221f787227adaff7398dc43e453847
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-06 19:40:08 +0800

    Test depth-space conversion

---
+++ template/DepthToSpace.cpp
@@ -0,0 +1,23 @@
+#include "common.hpp"
+
+{% set blocksize = numpy.random.randint(2, 6) -%}
+{% set deep = numpy.random.randint(2, 6, 4) * (1, blocksize * blocksize, 1, 1) -%}
+{% set size = numpy.prod(deep) -%}
+{% set x = numpy.arange(size, dtype=numpy.float32).reshape(deep) -%}
+{% set y = ("DepthToSpace" | infer(x, blocksize=blocksize))[0] -%}
+
+SKYPAT_F(DepthToSpace, image)
+{
+    const float x[] = {{ x.flatten() | array }};
+    const float y[] = {{ y.flatten() | array }};
+
+    const std::int32_t deep[] = {{ deep | array }};
+    const std::int32_t wide[] = {{ y.shape | array }};
+    
+    float buffer[{{ size }}];
+
+    ONNC_RUNTIME_depthtospace_float(nullptr, x, 4, deep, buffer, 4, wide, {{ blocksize }});
+    ASSERT_FALSE(std::memcmp(buffer, y, sizeof(y)));
+}
+{# vim: set ft=liquid: #}
+
---
+++ template/SpaceToDepth.cpp
@@ -0,0 +1,23 @@
+#include "common.hpp"
+
+{% set blocksize = numpy.random.randint(2, 6) -%}
+{% set wide = numpy.random.randint(2, 6, 4) * (1, 1, blocksize, blocksize) -%}
+{% set size = numpy.prod(wide) -%}
+{% set x = numpy.arange(size, dtype=numpy.float32).reshape(wide) -%}
+{% set y = ("SpaceToDepth" | infer(x, blocksize=blocksize))[0] -%}
+
+SKYPAT_F(SpaceToDepth, image)
+{
+    const float x[] = {{ x.flatten() | array }};
+    const float y[] = {{ y.flatten() | array }};
+
+    const std::int32_t wide[] = {{ wide | array }};
+    const std::int32_t deep[] = {{ y.shape | array }};
+    
+    float buffer[{{ size }}];
+
+    ONNC_RUNTIME_spacetodepth_float(nullptr, x, 4, wide, buffer, 4, deep, {{ blocksize }});
+    ASSERT_FALSE(std::memcmp(buffer, y, sizeof(y)));
+}
+{# vim: set ft=liquid: #}
+
