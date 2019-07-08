af727ab5a1366efea917f2cb7e9b8797fa01d952
Chen-Pang He <chen.pang.he@jdh8.org>
2019-07-09 00:17:09 +0800

    Complete test for LpPool

--- template/LpPool.cpp
+++ template/LpPool.cpp
@@ -9,14 +9,38 @@ SKYPAT_F(LpPool, {{ name }})
     {% set pads = numpy.minimum(numpy.tile(kernel - 1, 2), numpy.random.randint(0, 8, 2 * ndim)) -%}
     {% set strides = numpy.random.randint(1, 4, ndim) -%}
     {% set x = numpy.random.standard_cauchy(numpy.concatenate((channels, image))).astype(numpy.float32) -%}
-    {% set (y,) = "LpPool" | infer(x, kernel_shape=kernel, pads=pads, strides=strides) -%}
 
     const char autopad[] = "NOTSET";
-    const std::int32_t kernel[] = {{ kernel | array }};
-    const std::int32_t pads[] = {{ pads | array }};
-    const std::int32_t strides[] = {{ strides | array }};
+
+    std::int32_t kernel[] = {{ kernel | array }};
+    std::int32_t pads[] = {{ pads | array }};
+    std::int32_t strides[] = {{ strides | array }};
+    const std::int32_t shape[] = {{ x.shape | array }};
+
+    const std::size_t rows = {{ numpy.prod(channels) }};
+    const std::size_t ndim = {{ ndim }};
+
     const float x[] = {{ x.flatten() | array }};
-    const float y[] = {{ y.flatten() | array }};
+
+    {% for p in (1, 2, 3) -%}
+    {
+        {% set (y,) = "LpPool" | infer(x, kernel_shape=kernel, pads=pads, strides=strides) -%}
+
+        const float y[] = {{ y.flatten() | array }};
+        const std::int32_t reduced[] = {{ y.shape | array }};
+        const std::size_t cols = {{ numpy.prod(y.shape[2:]) }};
+        const auto matrix = reinterpret_cast<const float (*)[cols]>(y);
+        float buffer[rows][cols];
+
+        ONNC_RUNTIME_lppool_float(nullptr, x, ndim + 2, shape, *buffer, ndim + 2, reduced,
+            autopad, kernel, ndim, {{ p }}, pads, 2 * ndim, strides, ndim);
+
+        ASSERT_TRUE(std::equal(matrix, matrix + rows, buffer, [cols](const float* actual, const float* estimate)
+        {
+            return dragonite::norm(actual, estimate, cols) <= 1e-5 * dragonite::norm(actual, cols);
+        }));
+    }
+    {% endfor -%}
 }
 {% endcall -%}
 {# vim: set ft=liquid: #}
